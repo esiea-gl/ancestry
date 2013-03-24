67b284a4242e2cb98d98d756296916533e661e1b
QC <chouleur@et.esiea.fr>
2013-03-24 21:51:15 +0100

    Modification du projet pour être build depuis maven sans eclipse.

--- ancestry/src/test/java/fr/esiea/ancestry/domain/PersonTest.java
+++ ancestry/src/test/java/fr/esiea/ancestry/domain/PersonTest.java
@@ -1,7 +1,6 @@
 package fr.esiea.ancestry.domain;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.Calendar;
 import java.util.Date;
